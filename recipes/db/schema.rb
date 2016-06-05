5801e091a2394cd7f34dee8ea04fca1168a8c3d7
Amy Schlesener <amyschlesener@gmail.com>
2016-06-04 18:02:57 -0700

    Add amount to recipe ingredients

--- recipes/db/schema.rb
+++ recipes/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160530064218) do
+ActiveRecord::Schema.define(version: 20160605010009) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160530064218) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "unit_id"
+    t.string   "amount"
   end
 
   add_index "recipe_ingredients", ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
